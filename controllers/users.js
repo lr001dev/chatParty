99771093ffa3be891148c6f2f2e79db622b67a6a
Luis Rosario <luisrosariomix@gmail.com>
2019-06-01 22:14:39 -0700

    Added some comments and documentation throughout

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,21 @@
-const express = require(`express`)
-const router = express.Router()
-const User = require(`../models/users.js`)
-const bcrypt = require(`bcrypt`)
+const express = require(`express`) // Require Express
+const router = express.Router() //Grab Router Method From Express
+const User = require(`../models/users.js`) //Require User Model
+const bcrypt = require(`bcrypt`) //Require Hash & Salt Password Encryption
 
+//Create Route Show Route
 router.get(`/`, (req,res) => {
   res.render(`users/new.ejs`)
 })
 
+//Create New User
 router.post(`/`, (req,res) => {
+  
+  //Setting Password Encryption
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
   console.log(req.body)
   res.redirect(`/`)
 })
 
+//Export route to server.js
 module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,52 @@
+///////////////////////////////////////////////////
+/// Server.js Defines Our Routes, Controllers ///
+///          And Database Connections        ///
+////////////////////////////////////////////////
+
+//Enviromental Variables
 require(`dotenv`).config()
+
+//Express Setup
 const express = require(`express`)
 const app = express()
+
+//Port Number Setup
 const port = process.env.PORT
+
+//Mogoose Setup
 const mongoose = require(`mongoose`)
+
+//Middleware Declaration for PUT & DELETE Routes
 const methodOverride = require(`method-override`)
+
+//Importing Our Controllers
 const usersController = require(`./controllers/users.js`)
+
+///////////////////////
+//// Middleware //////
+/////////////////////
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(methodOverride(`_method`))
 
+/////////////////////////////////
+//// Paths To Controllers //////
+///////////////////////////////
+
 app.use(`/create-account`, usersController)
+
+////////////////////////
+//// Index Route //////
+///////////////////////
 
 app.get(`/`, (req,res) => {
   res.send(`Hello World`)
 })
+
+////////////////////////////////
+//// Listening To Server //////
+//////////////////////////////
 
 app.listen(port, () => {
   console.log(`I'm listening to port ${ port }`)
