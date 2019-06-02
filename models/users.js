0e1264384dcf9793da638232d7a0650d09076b7b
Luis Rosario <luisrosariomix@gmail.com>
2019-06-01 21:13:14 -0700

    Created User Model

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require(`mongoose`)
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  username: { type: String, require: true },
+  password: { type: String, require: true },
+  name: { type: String, require: true },
+  bio: String,
+  partyRooms: []
+})
+
+const User = mongoose.model(`User`, userSchema)
+module.exports = User
