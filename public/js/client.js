37591bd502230177664c4a9c45718efe92a2aeae
Luis Rosario <luisrosariomix@gmail.com>
2019-06-04 18:21:37 -0700

    Fixed deleted client.js

---
+++ public/js/client.js
@@ -0,0 +1,22 @@
+console.log(`loaded client.js`)
+const socket = io()
+
+$(() => {
+	  //Lets send a message to server
+	  $(`#chat`).submit((e) => {
+	  e.preventDefault()
+	  socket.emit(`chat message`, $(`#m`).val())
+	  socket.emit(`chat message`, $(`#m`).val(),room)
+	  // io.to(room).emit(`chat message`, $(`#m`).val())
+	  // io.to(room).emit(`chat message`, $(`#m`).val())
+	  $(`#m`).val(``)
+	  //Not sure why we are returning false
+	  return false
+	  })
+	  socket.on(`chat message`, (msg) => {
+	      //Looking to see if we get message back from server
+	      console.log(msg)
+	      $(`#messages`).append($(`<li>`).text(msg))
+
+	    })
+	})
